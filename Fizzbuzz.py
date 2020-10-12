1eb0dee71cad722f6d99a1d4cb259789ffddea67
Xancett <ryan.j@epoxstudios.com>
2020-10-12 19:24:24 -0400

    Initial test run

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,2 +1,12 @@
 def FizzBuzz(n):
-	return n
+	list = []
+	for i in range(1, n + 1):
+		if (i % 15 == 0):
+			list.append("FizzBuzz")
+		elif (i % 5 == 0):
+			list.append("Buzz")
+		elif( i % 3 == 0):
+			list.append("Fizz")
+		else:
+			list.append(str(n))
+	return list
