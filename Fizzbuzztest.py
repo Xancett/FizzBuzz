3505e1e90847963c420f5920e4fb7935c97a47fe
Xancett <ryan.j@epoxstudios.com>
2020-10-12 19:15:48 -0400

    Created unit tests for method

--- Fizzbuzztest.py
+++ Fizzbuzztest.py
@@ -4,7 +4,19 @@ import unittest
 class TestFizzbuzz(unittest.TestCase):
 
 	def test_one(self):
-		pass
+		self.assertEqual("1", FizzBuzz(1))
+	
+	def test_two(self):
+		self.assertEqual(["1", "2"], FizzBuzz(2))
+	
+	def test_three(self):
+		self.assertEqual(["1", "2", "Fizz"], FizzBuzz(3))
+	
+	def test_six(self):
+		self.assertEqual(["1", "2", "Fizz", "4", "Buzz", "Fizz"], FizzBuzz(6))
+	
+	def test_fifteen(self):
+		self.assertEqual(["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"], FizzBuzz(15))
 
 if __name__ == "__main__":
 	unittest.main()
